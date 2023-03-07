6a25ce06797261cbae8be6c8e9dbb17c67aef17e
Julieta Correa <mjcorrea210@gmail.com>
2023-03-06 21:12:50 -0300

    borre ejercicio 9

--- js/exercise9.js
+++
@@ -1,8 +0,0 @@
-let text = prompt("Escribe una frase");
-let nText = text.length;
-let i;
-for (i = 0; i < nText; i++) {
-if (text.substr(i,1) === "a" || text.substr(i,1) === "e" || text.substr(i,1) === "i" || text.substr(i,1) === "o" || text.substr(i,1) === "u") {
-document.write(text.substr(i,1);
-}
-}
